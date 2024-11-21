87febda8f77d937f7b972929717ec101596809ef
samanthagc <samgomescosta@gmail.com>
2024-11-20 21:56:00 -0300

    feat: implements middleware for resource not found

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
  const express = require ('express')
  const app = express()
 
- mid = (req, res) => {
-    res.send ('Hello Word Node.js community!')
- }
+ app.get ('/', (req, res, next) => {
+   console.log (new Date().toLocaleDateString(), req.method, req.path)
 
- app.get ('/', mid)
+   next()
+})
+
+app.get('/', (req, res) => {
+   res.send ('Hello Word')
+})
+
+app.use ((req, res) => {
+   res.status (404).send ('Recurso não foi encontrado')
+})
  
  app.listen (3000, () => {
     console.log ('Server is running on http://localhost:3000')
