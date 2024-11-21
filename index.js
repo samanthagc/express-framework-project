be7568e82466eefe5fc6fb9c98f15ad68720ab5a
samanthagc <samgomescosta@gmail.com>
2024-11-20 22:10:40 -0300

    feat: middleware static

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
  const express = require ('express')
  const app = express()
 
- app.get ('/', (req, res, next) => {
+ app.use ('/', (req, res, next) => {
    console.log (new Date().toLocaleDateString(), req.method, req.path)
 
    next()
 })
 
-app.get('/', (req, res) => {
-   res.send ('Hello Word')
-})
-
-app.use ((req, res) => {
-   res.status (404).send ('Recurso não foi encontrado')
-})
+app.use ('/site', express.static ('public'))
  
  app.listen (3000, () => {
     console.log ('Server is running on http://localhost:3000')
