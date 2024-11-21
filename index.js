f9d2d4c02d8792aad87d6b25a5d74880eee54bbc
samanthagc <samgomescosta@gmail.com>
2024-11-20 22:21:53 -0300

    feat: parser middlewear

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
  const express = require ('express')
  const app = express()
 
+app.use (express.json())
+
  app.use ('/', (req, res, next) => {
    console.log (new Date().toLocaleDateString(), req.method, req.path)
 
@@ -9,6 +11,12 @@
 
 app.use ('/site', express.static ('public'))
  
- app.listen (3000, () => {
-    console.log ('Server is running on http://localhost:3000')
- })
+app.post('/users', (req, res) => {
+   dados = req.body
+   console.log(dados)
+   res.send (`Informação recebida com sucesso. Seja bem vindo ${dados.nome}`)
+})
+
+app.listen (3000, () => {
+   console.log ('Server is running on http://localhost:3000')
+})
